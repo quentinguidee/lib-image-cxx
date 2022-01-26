db52aecab0d378c7b903ad1f1e73a7fac3c580b6
Quentin Guidée <quentin.guidee@gmail.com>
2022-01-26 17:52:13 +0100

    Separate QOI and Common tests
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

---
+++ tests/common_tests.cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <fstream>
+#include <utility>
+
+#include "gtest/gtest.h"
+#include "qoi_chunks.hpp"
+#include "qoi_image.hpp"
+#include "stream.hpp"
+
+TEST(CommonTest, BufferStream)
+{
+    BufferStream stream;
+
+    stream.write_8(0);
+    stream.write_8(12);
+    stream.write_8(42);
+
+    EXPECT_EQ(stream.size(), 3);
+
+    EXPECT_EQ(stream.peek_8(), 0);
+    EXPECT_EQ(stream.read_8(), 0);
+    EXPECT_EQ(stream.read_8(), 12);
+    EXPECT_EQ(stream.peek_8(), 42);
+    EXPECT_EQ(stream.read_8(), 42);
+}
+
+TEST(CommonTest, FileStream)
+{
+    OutputFileStream out("temp");
+
+    EXPECT_TRUE(out.is_open());
+
+    out.write_8(0);
+    out.write_8(12);
+    out.write_8(42);
+
+    out.close();
+
+    InputFileStream in("temp");
+
+    EXPECT_TRUE(in.is_open());
+
+    EXPECT_EQ(in.size(), 3);
+
+    EXPECT_EQ(in.peek_8(), 0);
+    EXPECT_EQ(in.read_8(), 0);
+    EXPECT_EQ(in.read_8(), 12);
+    EXPECT_EQ(in.peek_8(), 42);
+    EXPECT_EQ(in.read_8(), 42);
+
+    in.close();
+
+    remove("temp.qoi");
+}
--- tests/qoi_tests.cpp
+++ tests/qoi_tests.cpp
@@ -168,52 +168,6 @@ TEST(QOIFormatTest, Footer)
     EXPECT_EQ(new_footer, QOI::Footer());
 }
 
-TEST(QOIFormatTest, BufferStream)
-{
-    BufferStream stream;
-
-    stream.write_8(0);
-    stream.write_8(12);
-    stream.write_8(42);
-
-    EXPECT_EQ(stream.size(), 3);
-
-    EXPECT_EQ(stream.peek_8(), 0);
-    EXPECT_EQ(stream.read_8(), 0);
-    EXPECT_EQ(stream.read_8(), 12);
-    EXPECT_EQ(stream.peek_8(), 42);
-    EXPECT_EQ(stream.read_8(), 42);
-}
-
-TEST(QOIFormatTest, FileStream)
-{
-    OutputFileStream out("temp.qoi");
-
-    EXPECT_TRUE(out.is_open());
-
-    out.write_8(0);
-    out.write_8(12);
-    out.write_8(42);
-
-    out.close();
-
-    InputFileStream in("temp.qoi");
-
-    EXPECT_TRUE(in.is_open());
-
-    EXPECT_EQ(in.size(), 3);
-
-    EXPECT_EQ(in.peek_8(), 0);
-    EXPECT_EQ(in.read_8(), 0);
-    EXPECT_EQ(in.read_8(), 12);
-    EXPECT_EQ(in.peek_8(), 42);
-    EXPECT_EQ(in.read_8(), 42);
-
-    in.close();
-
-    remove("temp.qoi");
-}
-
 TEST(QOIFormatTest, Color)
 {
     QOI::Color color{255, 1, 99, 255};
