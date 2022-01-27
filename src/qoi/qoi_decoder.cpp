3a97576035ce7b6adc292223dad8d857b75d854f
Quentin Guidée <quentin.guidee@gmail.com>
2022-01-27 10:58:44 +0100

    QOI: Simplify decode loop
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/qoi/qoi_decoder.cpp
+++ src/qoi/qoi_decoder.cpp
@@ -6,8 +6,6 @@
 
 void QOI::Decoder::decode(InputStream &in, RawImage &image) const
 {
-    long size = in.size() - Header::SIZE - Footer::SIZE;
-
     Header header(in);
 
     image.width = header.get_width();
@@ -21,32 +19,28 @@ void QOI::Decoder::decode(InputStream &in, RawImage &image) const
     Color previously_seen_pixels[64];
     Color previous_color;
 
-    long i = 0;
-    while (i < size)
+    uint8_t run = 0;
+    long image_size = image.width * image.height;
+    for (long i = 0; i < image_size; ++i)
     {
         uint8_t tag_8 = in.peek_8();
         uint8_t tag_2 = tag_8 & 0xc0;
 
-        if (tag_8 == RGB::TAG)
+        if (run > 0)
+        {
+            --run;
+        }
+        else if (tag_8 == RGB::TAG)
         {
             previous_color = RGB(in).get_color();
-            previously_seen_pixels[previous_color.hash()] = previous_color;
-            image.pixels.push_back(previous_color.to_pixel());
-            i += RGB::SIZE;
         }
         else if (tag_8 == RGBA::TAG)
         {
             previous_color = RGBA(in).get_color();
-            previously_seen_pixels[previous_color.hash()] = previous_color;
-            image.pixels.push_back(previous_color.to_pixel());
-            i += RGBA::SIZE;
         }
         else if (tag_2 == Index::TAG)
         {
-            uint8_t index = Index(in).get_index();
-            previous_color = previously_seen_pixels[index];
-            image.pixels.push_back(previous_color.to_pixel());
-            i += Index::SIZE;
+            previous_color = previously_seen_pixels[Index(in).get_index()];
         }
         else if (tag_2 == Diff::TAG)
         {
@@ -55,10 +49,6 @@ void QOI::Decoder::decode(InputStream &in, RawImage &image) const
             previous_color.r += diff.get_diff_red();
             previous_color.g += diff.get_diff_green();
             previous_color.b += diff.get_diff_blue();
-
-            previously_seen_pixels[previous_color.hash()] = previous_color;
-            image.pixels.push_back(previous_color.to_pixel());
-            i += Diff::SIZE;
         }
         else if (tag_2 == Luma::TAG)
         {
@@ -67,21 +57,16 @@ void QOI::Decoder::decode(InputStream &in, RawImage &image) const
             previous_color.r += luma.get_diff_green() + luma.get_diff_red_green();
             previous_color.g += luma.get_diff_green();
             previous_color.b += luma.get_diff_green() + luma.get_diff_blue_green();
-
-            previously_seen_pixels[previous_color.hash()] = previous_color;
-            image.pixels.push_back(previous_color.to_pixel());
-            i += Luma::SIZE;
         }
         else if (tag_2 == Run::TAG)
         {
-            uint8_t repeat = Run(in).get_run();
-            for (uint8_t j = 0; j < repeat; ++j)
-                image.pixels.push_back(previous_color.to_pixel());
-            i += Run::SIZE;
+            run = Run(in).get_run() - 1;
         }
         else
         {
             throw DecodeException("Tag not recognized.");
         }
+        previously_seen_pixels[previous_color.hash()] = previous_color;
+        image.pixels.push_back(previous_color.to_pixel());
     }
 }
