7eebcc685a09a7070e9706c88f8e2c886c3694f9
Quentin Guidée <quentin.guidee@gmail.com>
2022-02-13 23:06:57 +0100

    Remove deprecated includes in formats definitions
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/bmp/bmp_format.hpp
+++ src/bmp/bmp_format.hpp
@@ -1,11 +1,5 @@
 #pragma once
 
-#include <memory>
-
-#include "bmp_decoder.hpp"
-#include "bmp_encoder.hpp"
-#include "decoder.hpp"
-#include "encoder.hpp"
 #include "format.hpp"
 
 static const Format BMP_FORMAT{
--- src/qoi/qoi_format.hpp
+++ src/qoi/qoi_format.hpp
@@ -1,12 +1,6 @@
 #pragma once
 
-#include <memory>
-
-#include "decoder.hpp"
-#include "encoder.hpp"
 #include "format.hpp"
-#include "qoi_decoder.hpp"
-#include "qoi_encoder.hpp"
 
 static const Format QOI_FORMAT{
     "qoi",
