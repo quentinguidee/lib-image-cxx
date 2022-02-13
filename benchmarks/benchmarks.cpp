cf32a18ab5ed81bc6d8d2bc3520f7de6b698fa71
Quentin Guidée <quentin.guidee@gmail.com>
2022-02-13 14:28:32 +0100

    Add BMP to benchmarks
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- benchmarks/benchmarks.cpp
+++ benchmarks/benchmarks.cpp
@@ -1,8 +1,11 @@
 #include "benchmark/benchmark.h"
+#include "bmp_decoder.hpp"
+#include "bmp_encoder.hpp"
 #include "common.hpp"
 #include "qoi_decoder.hpp"
 #include "qoi_encoder.hpp"
 
 BENCHMARK_IMAGE(QOI, "image_1.qoi");
+BENCHMARK_IMAGE(BMP, "image_1.bmp");
 
 BENCHMARK_MAIN();
